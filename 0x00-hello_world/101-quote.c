b2ea56bc23344cf9f217d85128f91d5de21e59f5
nourhanWael <nourhan.wael.29.3.com>
2023-03-20 16:18:32 -0700

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Entry point 
  *
- * Return: Always 0 (Success)
+ * Description: print a qoute using write function
+ *      write(int fd,const void *buf,size_t count)
+ *
+ * Return: 1  (not Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+            
+	write(1,quo,59);
 	return (1);
 }
 
